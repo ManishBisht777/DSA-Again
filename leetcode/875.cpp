d8f751640c2e7caa3f228a894c33d58918d3111a
manish <Manishbisht9711@gmail.com>
2022-01-20 19:22:16 +0530

    leetcode 875

--- leetcode/875.cpp
+++ leetcode/875.cpp
@@ -1,1 +1,33 @@
 // koko eating bananas
+
+int minEatingSpeed(vector<int> &piles, int h)
+{
+    int st, end, mid;
+    st = 1;
+    int n = piles.size();
+
+    int count;
+
+    for (int i = 0; i < n; i++)
+    {
+        end = max(piles[i], end);
+    }
+
+    while (st < end)
+    {
+        mid = st + (end - st) / 2;
+        count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            count += (piles[i] + mid - 1) / mid;
+        }
+
+        if (count > h)
+            st = mid + 1;
+        else
+            end = mid;
+    }
+
+    return st;
+}
