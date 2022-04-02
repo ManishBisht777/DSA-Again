e50f2035a4a62834fa184c9349025aab4060c799
manish <Manishbisht9711@gmail.com>
2022-04-02 10:58:40 +0530

    top view of binary tree

---
+++ gfg/topview.cpp
@@ -0,0 +1,33 @@
+vector<int> topView(Node *root)
+{
+    vector<int> ans;
+
+    map<int, int> mp;
+    queue<pair<Node *, int>> todo;
+
+    todo.push({root, 0});
+
+    while (!todo.empty)
+    {
+        auto it = todo.front();
+        todo.pop();
+
+        Node *node = it.first;
+        int line = it.second;
+
+        if (mp.find(line) == mp.end())
+            mp[line] = node->data;
+
+        if (node->left)
+            todo.push({node->left, line - 1});
+        if (node->right)
+            todo.push({node->right, line + 1});
+    }
+
+    for (auto it : mp)
+    {
+        ans.push_back(it.second);
+    }
+
+    return ans;
+}
