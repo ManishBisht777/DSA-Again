68a42545cb45691da334bf5447aff98d0fb0973c
manish <Manishbisht9711@gmail.com>
2022-04-04 10:59:17 +0530

    1721. Swapping Nodes in a Linked List

---
+++ leetcode/questions/1721.cpp
@@ -0,0 +1,23 @@
+ListNode* swapNodes(ListNode* head, int k) {
+        
+        ListNode* ptr1=head,*ptr2=head,*kth=head;
+        
+        while(--k)
+        {
+            ptr1=ptr1->next;
+        }
+        
+        kth=ptr1;
+        ptr1=ptr1->next;
+        
+        while(ptr1)
+        {
+            ptr1=ptr1->next;
+            ptr2=ptr2->next;
+        }
+        
+        swap(kth->val,ptr2->val);
+        
+        return head;
+        
+    }
