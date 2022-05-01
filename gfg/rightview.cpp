865771a485295f0f339554dc4fae26627e55927d
manish <Manishbisht9711@gmail.com>
2022-05-01 16:06:42 +0530

    right view of binary tree

---
+++ gfg/rightview.cpp
@@ -0,0 +1,19 @@
+void helper(Node *root, int lvl, vector<int> &ans)
+{
+    if (!root)
+        return;
+
+    if (lvl == ans.size())
+        ans.push_back(root->data);
+
+    helper(root->right, lvl + 1, ans);
+    helper(root->left, lvl + 1, ans);
+}
+
+vector<int> leftView(Node *root)
+{
+    // Your code here
+    vector<int> ans;
+    helper(root, 0, ans);
+    return ans;
+}
