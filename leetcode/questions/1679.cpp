97bfda05766be392b64027f469a2d0d171e725f5
manish <Manishbisht9711@gmail.com>
2022-05-04 11:10:51 +0530

    1679. Max Number of K-Sum Pairs [better T(N)]

--- leetcode/questions/1679.cpp
+++ leetcode/questions/1679.cpp
@@ -20,3 +20,24 @@ int maxOperations(vector<int> &nums, int k)
 
     return cnt;
 }
+
+// T(n) - O(N)
+// Space - O(N)
+
+int maxOperations(vector<int> &nums, int k)
+{
+    unordered_map<int, int> freq;
+    int ans = 0;
+    for (auto cur : nums)
+    {
+        int complement = k - cur;
+        if (freq[complement] > 0)
+        {
+            ans++;
+            freq[complement]--;
+        }
+        else
+            freq[cur]++;
+    }
+    return ans;
+}
