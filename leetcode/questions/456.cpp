786c225fe1a5f44f457700a66ba1fbdd1ecf7de6
manish <Manishbisht9711@gmail.com>
2022-05-07 13:08:40 +0530

    yo

--- leetcode/questions/456.cpp
+++ leetcode/questions/456.cpp
@@ -1,19 +1,18 @@
 bool find132pattern(vector<int> &nums)
 {
     int s3 = INT_MIN;
-    stack<int> s;
+    stack<int> st;
     for (int i = nums.size() - 1; i >= 0; i--)
     {
         if (nums[i] < s3)
             return true;
-
-        while (!s.empty() && nums[i] > s.top())
-        {
-            s3 = s.top();
-            s.pop(;
-        }
-        s.push(nums[i]);
+        else
+            while (!st.empty() && nums[i] > st.top())
+            {
+                s3 = st.top();
+                st.pop();
+            }
+        st.push(nums[i]);
     }
-
     return false;
 }
