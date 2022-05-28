f47d109e4321bd924ae9d96e7e4af887b4656ea4
manish <Manishbisht9711@gmail.com>
2022-05-28 12:34:16 +0530

    1192. Critical Connections in a Network [Tarjan's Algo]

---
+++ leetcode/questions/1192.cpp
@@ -0,0 +1,52 @@
+void dfs(int node, vector<int> &mit, vector<int> &low,
+         int parent, vector<vector<int>> &adj,
+         vector<bool> &vis,
+         vector<vector<int>> &ans)
+{
+    static int time = 0;
+    vis[node] = 1;
+    mit[node] = low[node] = time++;
+
+    for (auto it : adj[node])
+    {
+        if (it == parent)
+            continue;
+
+        if (!vis[it])
+        {
+            dfs(it, mit, low, node, adj, vis, ans);
+            low[node] = min(low[it], low[node]);
+            if (low[it] > mit[node])
+                ans.push_back({node, it});
+        }
+        else
+        {
+            low[node] = min(low[node], mit[it]);
+        }
+    }
+}
+
+vector<vector<int>> criticalConnections(int n, vector<vector<int>> &connections)
+{
+
+    vector<bool> vis(n, false);
+    vector<int> mit(n, 0), low(n, 0);
+    vector<vector<int>> adj(n, vector<int>()), ans;
+
+    // make adj list
+    for (auto it : connections)
+    {
+        adj[it.front()].push_back(it.back());
+        adj[it.back()].push_back(it.front());
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            dfs(i, mit, low, -1, adj, vis, ans);
+        }
+    }
+
+    return ans;
+}
