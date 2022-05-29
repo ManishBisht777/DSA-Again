d27d79a04b8cbb5bf61adc52edbcdaed8be67046
manish <Manishbisht9711@gmail.com>
2022-05-29 09:38:02 +0530

    2287. Rearrange Characters to Make Target String

---
+++ leetcode/questions/2287.cpp
@@ -0,0 +1,14 @@
+int rearrangeCharacters(string s, string target) {
+        const int maxd = 26;
+        vector<int> A(maxd), B(maxd);
+        for(char ch: s)
+            ++A[ch - 'a'];
+        for(char ch: target)
+            ++B[ch - 'a'];
+        int ans = INT_MAX;
+        for(int i = 0; i < maxd; ++i)
+            if(B[i])
+                ans = min(ans, A[i] / B[i]);
+        return ans;
+        
+    }
