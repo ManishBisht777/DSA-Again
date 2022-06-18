0b681b16a9a073c0ea04a5c56c8d646306e323d0
manish <Manishbisht9711@gmail.com>
2022-06-18 13:07:03 +0530

    optimal sol

--- gfg/array/median2SortedArray.cpp
+++ gfg/array/median2SortedArray.cpp
@@ -23,3 +23,34 @@ double MedianOfArrays(vector<int> &arr1, vector<int> &arr2)
         return (double)merge[n / 2];
     return (double)(merge[n / 2] + merge[n / 2 - 1]) / 2;
 }
+
+// optimal sol
+
+double MedianOfArrays(vector<int> &arr1, vector<int> &arr2)
+{
+    // Your code goes here
+    int i = 0, j = 0;
+    int m1 = -1, m2 = -1;
+    int n = arr1.size();
+    int m = arr2.size();
+
+    for (int cnt = 0; cnt <= (m + n) / 2; cnt++)
+    {
+        m2 = m1;
+
+        if (i != n && j != m)
+        {
+            m1 = (arr1[i] > arr2[j]) ? arr2[j++] : arr1[i++];
+        }
+        else if (i < n)
+        {
+            m1 = arr1[i++];
+        }
+        else
+            m1 = arr2[j++];
+    }
+
+    if ((m + n) % 2 == 1)
+        return m1;
+    return (double)(m1 + m2) / 2;
+}
