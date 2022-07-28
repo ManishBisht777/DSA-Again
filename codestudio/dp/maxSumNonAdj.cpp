496dad1d07bea348509d56fe3e5e2af8f058a6b5
manish <Manishbisht9711@gmail.com>
2022-07-28 18:20:24 +0530

    max sum not adj (Tabulation and space optimization)

--- codestudio/dp/maxSumNonAdj.cpp
+++ codestudio/dp/maxSumNonAdj.cpp
@@ -19,3 +19,49 @@ int maximumNonAdjacentSum(vector<int> &nums)
     int ans = helper(nums, 0, dp);
     return ans;
 }
+
+// tabulation
+
+int maximumNonAdjacentSum(vector<int> &nums)
+{
+    // Write your code here.
+    vector<int> dp(nums.size(), 0);
+    dp[0] = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        int take = 0;
+        if (i > 1)
+            take = nums[i] + dp[i - 2];
+        else
+            take = nums[i];
+
+        int nottake = dp[i - 1];
+
+        dp[i] = max(take, nottake);
+    }
+    return dp[nums.size() - 1];
+}
+
+// space optimization
+
+int maximumNonAdjacentSum(vector<int> &nums)
+{
+    // Write your code here.
+    int prev = nums[0], prev2 = 0;
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        int take = nums[i];
+        if (i > 1)
+            take += prev2;
+
+        int nottake = prev;
+
+        int curr = max(take, nottake);
+
+        prev2 = prev;
+        prev = curr;
+    }
+    return prev;
+}
