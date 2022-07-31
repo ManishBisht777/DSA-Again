c765a3163ab5e1db3496dd2bd7c2599a4c47992f
manish <Manishbisht9711@gmail.com>
2022-07-31 13:33:14 +0530

    space optimization max sum path matrix

--- codestudio/dp/maxPathMatrix.cpp
+++ codestudio/dp/maxPathMatrix.cpp
@@ -77,3 +77,50 @@ int getMaxPathSum(vector<vector<int>> &matrix)
 
     return maxi;
 }
+
+// space optimization
+
+int getMaxPathSum(vector<vector<int>> &matrix)
+{
+    //  Write your code here.
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    vector<int> prev(m, 0), curr(m, 0);
+
+    for (int i = 0; i < m; i++)
+    {
+        prev[i] = matrix[0][i];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int down = matrix[i][j] + prev[j];
+
+            int left = matrix[i][j];
+            if (j - 1 >= 0)
+                left += prev[j - 1];
+            else
+                left += -1e9;
+
+            int right = matrix[i][j];
+            if (j + 1 < m)
+                right += prev[j + 1];
+            else
+                right += -1e9;
+
+            curr[j] = max(down, max(left, right));
+        }
+        prev = curr;
+    }
+
+    int maxi = INT_MIN;
+    for (int i = 0; i < m; i++)
+    {
+        maxi = max(maxi, prev[i]);
+    }
+
+    return maxi;
+}
